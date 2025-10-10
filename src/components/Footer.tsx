6555564e883852ad84504c93f06df41b901c1e28
Celestine <ejioforcelestine77@gmail.com>
2025-10-10 06:49:27 +0100

    Refactor Footer component layout and adjust CSS styles for improved design
    - Updated padding in the Footer component for better spacing and alignment of elements.
    - Adjusted width of the navigation list for a more balanced layout.
    - Reduced padding for icon list items to enhance visual consistency.
    - Added top padding to the footer text for improved readability.
    - Cleaned up commented-out outline styles in index.css for a cleaner design.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Logo from './Logo';
 export default function Footer() {
   return (
     <section className="h-[30rem] text-white bg-black -mx-25 px-25 border-t border-white">
-      <div className="flex justify-between items-center h-1/2">
+      <div className="flex justify-between items-center h-1/2 pt-20">
         <Logo />
-        <ul className="flex justify-between w-1/2">
+        <ul className="flex justify-between w-2/5">
           <li>
             <p>Home</p>
           </li>
@@ -21,19 +21,19 @@ export default function Footer() {
         </ul>
         <div>
           <ul className="flex items-center w-full">
-            <li className="px-4">
+            <li className="px-2">
               <img src="./x.svg" alt="x" />
             </li>
-            <li className="px-4">
+            <li className="px-2">
               <img src="./footer-mail.svg" alt="mail" />
             </li>
-            <li className="px-4">
+            <li className="px-2">
               <img src="./telegram.svg" alt="telegram" />
             </li>
           </ul>
         </div>
       </div>
-      <p className="text-4xl text-center py-10">© Cyberverse Labs 2025 — Build what’s next now.</p>
+      <p className="text-4xl text-center pt-20">© Cyberverse Labs 2025 — Build what’s next now.</p>
     </section>
   );
 }
