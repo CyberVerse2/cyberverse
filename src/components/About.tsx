4a081ae90ed1823f3427942243a81b71b858f773
Celestine <ejioforcelestine77@gmail.com>
2025-10-10 08:11:07 +0100

    Refactor About and Products components for improved styling and responsiveness
    - Updated the About component to enhance text sizes and padding for better readability and visual appeal.
    - Adjusted the Products component to improve text alignment and consistency in font sizes across different screen sizes.
    - Enhanced image styling in both components for a more cohesive design.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
 export default function About() {
   return (
     <section className="min-h-screen w-full flex justify-center items-center px-4 md:px-0">
-      <div className="text-white rounded-[2rem] md:rounded-[4.4rem] bg-gradient-to-br from-[#1E1E1E] to-[#03080F] min-h-[calc(100vh-20rem)] flex flex-col md:flex-row justify-between items-center md:items-end p-8 md:p-20 gap-8 md:gap-y-10">
+      <div className="text-white rounded-[2rem] md:rounded-[4.4rem] bg-gradient-to-br from-[#1E1E1E] to-[#03080F] min-h-[calc(100vh-20rem)] flex flex-col md:flex-row justify-between items-center p-6 md:p-20 gap-8 md:gap-y-10">
         <div className="md:ml-30 w-full md:w-[90%]">
-          <h2 className="text-6xl md:text-9xl font-extrabold pb-3">About Us</h2>
-          <p className="text-2xl md:text-[2.8rem] w-full md:w-3/4 pt-5 font-normal">
+          <h2 className="text-5xl md:text-9xl font-extrabold pb-3">About Us</h2>
+          <p className="text-xl md:text-[2.8rem] w-full md:w-3/4 pt-5 font-normal">
             At Cyberverse Labs, We experiment, we ship, we break things, and we rebuild them better.
             Our playground is AI, crypto, and apps, but really, it's the edge of what's possible.
           </p>
@@ -15,7 +15,11 @@ export default function About() {
             Let's Build!
           </a>
         </div>
-        <img src="./about-img.png" alt="" className="w-full md:w-[75%] md:mr-15 self-end" />
+        <img
+          src="./about-img.png"
+          alt=""
+          className="w-[90%] md:w-[75%] md:mr-15 self-end max-w-md md:max-w-none"
+        />
       </div>
     </section>
   );
