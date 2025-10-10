6201de5c38d8bfff6dfc6311f4eb0ff4fb539398
Celestine <ejioforcelestine77@gmail.com>
2025-10-10 08:27:59 +0100

    Refactor Footer and Products components for improved styling and layout
    - Updated the Footer component to enhance layout, spacing, and text styles for better visual consistency.
    - Adjusted the Products component to improve the rounded corners of elements and overall design coherence.
    - Simplified text elements in the Footer for a cleaner presentation and improved readability.

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,8 +5,8 @@ export default function Products() {
     <section className="bg-[#fafafa] py-20 md:py-30 -mx-6 md:-mx-25 px-4 md:px-25">
       <div>
         <div>
-          <div className="w-[90%] md:w-3/4 lg:w-1/2 h-32 md:h-50 bg-white flex justify-between rounded-[3rem] md:rounded-[8rem] mx-auto p-2 md:p-6 mb-6 font-semibold">
-            <span className="w-1/2 h-full bg-[var(--button-color)] flex justify-center items-center text-white rounded-[2.5rem] md:rounded-[8rem] px-1 md:px-4">
+          <div className="w-[90%] md:w-3/4 lg:w-1/2 h-32 md:h-50 bg-white flex justify-between rounded-[8rem] mx-auto p-2 md:p-6 mb-6 font-semibold">
+            <span className="w-1/2 h-full bg-[var(--button-color)] flex justify-center items-center text-white rounded-[8rem] px-1 md:px-4">
               <p className="text-center leading-tight text-[1.6rem] md:text-5xl lg:text-6xl">
                 Things we've built
               </p>
